f5745a0e8ab0f693921075f600c9bd653a5e68da
Volodia Chornenkyy <chornenkyy@gmail.com>
2016-10-15 01:01:49 +0300

    add BottomNavigation and move logic to fragment

--- app/src/main/kotlin/com/vchornenkyy/whatpulsehelper/MainActivity.kt
+++ app/src/main/kotlin/com/vchornenkyy/whatpulsehelper/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.vchornenkyy.whatpulsehelper
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import com.vchornenkyy.whatpulsehelper.api.Cache
-import com.vchornenkyy.whatpulsehelper.api.InMemoryCache
-import com.vchornenkyy.whatpulsehelper.api.WhatPulseRestApi
-import com.vchornenkyy.whatpulsehelper.api.model.UserResponse
 import kotlinx.android.synthetic.main.activity_main.*
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,29 +11,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        jsonFromServer.setOnClickListener { loadUser() }
+        bottomBar.setOnTabSelectListener { tabId ->
+            when (tabId) {
+                R.id.tab_profile -> openFragment(GeneralInfoFragment.newInstance())
+                R.id.tab_computers -> openFragment(Fragment())
+                R.id.tab_teams -> openFragment(Fragment())
+            }
+        }
     }
 
-    private fun loadUser() {
-        val userApi = WhatPulseRestApi().userApi
-        val cache: Cache = InMemoryCache.instance
-        val username = "temnoi"
-        cache.getUser()
-                .switchIfEmpty(userApi.getUser(username))
-                .map { userResponse -> convert(userResponse) }
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { userResponse ->
-                    jsonFromServer.text = userResponse.toString()
-
-                    // todo take timestamp from 'GeneratedTime'
-                    cache.saveUser(userResponse, System.currentTimeMillis())
-                }
-    }
-
-    //TODO move it somewhere
-    fun convert(userResponse: UserResponse): UserResponse {
-        // prepare model for UI (for ex. format data/time)
-        return userResponse
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.mainActivityContainer, fragment)
+                .commit()
     }
 }
