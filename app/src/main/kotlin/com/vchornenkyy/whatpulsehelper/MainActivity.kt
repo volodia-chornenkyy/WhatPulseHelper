c9557c18963e2da53245846ff4159e7d1892309e
Volodia Chornenkyy <volodia.chornenkyy@techmagic.co>
2016-10-21 19:02:19 +0300

    use BottomNavigationView from design library 2

--- app/src/main/kotlin/com/vchornenkyy/whatpulsehelper/MainActivity.kt
+++ app/src/main/kotlin/com/vchornenkyy/whatpulsehelper/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.vchornenkyy.whatpulsehelper
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,12 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottomBar.setOnTabSelectListener { tabId ->
-            when (tabId) {
+        if (savedInstanceState == null) {
+            openFragment(GeneralInfoFragment.newInstance())
+        }
+
+        bottomBar.setOnNavigationItemSelectedListener {
+            item ->
+            when (item.itemId) {
                 R.id.tab_profile -> openFragment(GeneralInfoFragment.newInstance())
-                R.id.tab_computers -> openFragment(Fragment())
-                R.id.tab_teams -> openFragment(Fragment())
+                R.id.tab_computers, R.id.tab_teams -> Toast.makeText(this, "Coming soon", Snackbar.LENGTH_SHORT).show()
             }
+            return@setOnNavigationItemSelectedListener false
         }
     }
 
