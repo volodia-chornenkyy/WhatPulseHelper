2d3aa1ee66c7fedffa4bd598575666255f05e0ec
Volodia Chornenkyy <chornenkyy@gmail.com>
2016-10-29 23:59:25 +0300

    Add Crashlytics

--- app/src/main/kotlin/com/vchornenkyy/whatpulsehelper/App.kt
+++ app/src/main/kotlin/com/vchornenkyy/whatpulsehelper/App.kt
@@ -1,9 +1,13 @@
 package com.vchornenkyy.whatpulsehelper
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.answers.Answers
+import io.fabric.sdk.android.Fabric
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        Fabric.with(this, Crashlytics(), Answers())
     }
 }
