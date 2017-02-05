0ab264a9ce76ff8f79a3c0374a0ff22d8502dd95
Volodia Chornenkyy <chornenkyy@gmail.com>
2017-02-05 13:40:03 +0200

    Move error display to view

--- app/src/main/kotlin/com/vchornenkyy/whatpulsehelper/view/screens/login/LoginActivity.kt
+++ app/src/main/kotlin/com/vchornenkyy/whatpulsehelper/view/screens/login/LoginActivity.kt
@@ -2,7 +2,9 @@ package com.vchornenkyy.whatpulsehelper.view.screens.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.fasterxml.jackson.databind.JsonMappingException
 import com.vchornenkyy.whatpulsehelper.R
 import com.vchornenkyy.whatpulsehelper.common.helper.SharedPrefAppProperties
 import com.vchornenkyy.whatpulsehelper.domain.LoginUseCase
@@ -10,8 +12,10 @@ import com.vchornenkyy.whatpulsehelper.view.BaseActivity
 import com.vchornenkyy.whatpulsehelper.view.MainActivity
 import com.vchornenkyy.whatpulsehelper.view.tracking.EventTracker
 import kotlinx.android.synthetic.main.login_layout.*
+import java.net.UnknownHostException
 
 class LoginActivity : BaseActivity(), LoginPresenter.View {
+
 
     var presenter: LoginPresenter<LoginPresenter.View>? = null
 
@@ -52,5 +56,13 @@ class LoginActivity : BaseActivity(), LoginPresenter.View {
 
         startActivity(Intent(this, MainActivity::class.java))
         finish()
+    }
+
+    override fun displayError(e: Throwable) {
+        when (e) {
+            is UnknownHostException -> displayMessage("Please check internet connection")
+            is JsonMappingException -> displayMessage("Unknown user ID")
+            else -> Log.e(LoginActivity::class.java.canonicalName, e.message, e)
+        }
     }
 }
--- app/src/main/kotlin/com/vchornenkyy/whatpulsehelper/view/screens/login/LoginPresenter.kt
+++ app/src/main/kotlin/com/vchornenkyy/whatpulsehelper/view/screens/login/LoginPresenter.kt
@@ -1,13 +1,9 @@
 package com.vchornenkyy.whatpulsehelper.view.screens.login
 
-import android.util.Log
-import com.fasterxml.jackson.databind.JsonMappingException
 import com.vchornenkyy.whatpulsehelper.domain.BasePresenter
 import com.vchornenkyy.whatpulsehelper.domain.LoginUseCase
 import com.vchornenkyy.whatpulsehelper.view.BaseView
-import com.vchornenkyy.whatpulsehelper.view.screens.general_info.GeneralInfoPresenter
 import rx.Subscription
-import java.net.UnknownHostException
 
 class LoginPresenter<VIEW : LoginPresenter.View> constructor(private val loginUseCase: LoginUseCase) : BasePresenter<VIEW>() {
 
@@ -30,13 +26,7 @@ class LoginPresenter<VIEW : LoginPresenter.View> constructor(private val loginUs
                         { error ->
                             view?.showProgress(false)
 
-                            if (error is UnknownHostException) {
-                                view?.displayMessage("Please check internet connection")
-                            } else if (error is JsonMappingException) {
-                                view?.displayMessage("Unknown user ID")
-                            } else {
-                                Log.e(GeneralInfoPresenter::class.java.name, error.message, error)
-                            }
+                            view?.displayError(error)
                         }
                 )
     }
@@ -56,5 +46,7 @@ class LoginPresenter<VIEW : LoginPresenter.View> constructor(private val loginUs
         fun showProgress(show: Boolean)
 
         fun openMainScreen()
+
+        fun displayError(e: Throwable) // TODO move to BaseView in case if it will work well
     }
 }
