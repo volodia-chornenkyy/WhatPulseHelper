c478c305eb9690335c3bc5c2e782d4c3a026ddb5
Volodia Chornenkyy <volodia.chornenkyy@techmagic.co>
2017-02-13 19:02:03 +0200

    Skip CI build for test reports

--- uploadFailedTestReportToGit.sh
+++ uploadFailedTestReportToGit.sh
@@ -3,6 +3,6 @@
 if ! ./gradlew jacocoTestReport; then
     git checkout -b reports/test_fail;
     git add -f app/build/reports/tests/;
-    git commit -m "Failed tests report";
+    git commit -m "Failed tests report [ci skip]";
     git push --set-upstream origin reports/test_fail;
 fi
