8f08569a8251fcd73a0e2bb5002cbed9d95d2f7f
Volodia Chornenkyy <chornenkyy@gmail.com>
2017-02-14 23:56:06 +0200

    Clean up fragments little bit by moving smth to base fragment

--- app/src/main/kotlin/com/vchornenkyy/whatpulsehelper/view/BaseFragment.kt
+++ app/src/main/kotlin/com/vchornenkyy/whatpulsehelper/view/BaseFragment.kt
@@ -2,17 +2,34 @@ package com.vchornenkyy.whatpulsehelper.view
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 abstract class BaseFragment : Fragment(), BaseView {
     var sharedView: BaseView? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
 
         sharedView = SharedView(this)
+
+        initPresenter()
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        removePresenter()
     }
 
     override fun displayMessage(message: String) {
         sharedView?.displayMessage(message)
     }
+
+    protected open fun initPresenter() {}
+
+    protected open fun removePresenter() {}
 }
