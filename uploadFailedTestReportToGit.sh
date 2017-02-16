cd4bb10088c07d5fbdd603849a29cc846f1c9d50
Volodia Chornenkyy <chornenkyy@gmail.com>
2017-02-16 22:41:44 +0200

    Remove unneeded task from build process at CI

--- uploadFailedTestReportToGit.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if ! ./gradlew jacocoTestReport; then
-    git checkout -b reports/test_fail;
-    git add -f app/build/reports/tests/;
-    git commit -m "Failed tests report [ci skip]";
-    git push --set-upstream origin reports/test_fail;
-fi
